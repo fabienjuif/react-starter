f2e56ed5c5347f2b6df1be289d33072d451f1e03
Fabien JUIF <fabien.juif@zenika.com>
2016-08-15 16:06:51 +0200

    Sort list

--- src/components/List/List.container.js
+++ src/components/List/List.container.js
@@ -6,8 +6,13 @@ import { getList, getFilter } from 'redux/reducers'
 import { fetchList } from 'redux/actions'
 import Component from './List'
 
+const getSortedItems = createSelector(
+  [getList],
+  (list) => list.sort((l1, l2) => l1.name.first.localeCompare(l2.name.first))
+)
+
 const getFilteredItems = createSelector(
-  [getList, getFilter],
+  [getSortedItems, getFilter],
   (list, filter) => list
     .filter(l => `${l.name.first}${l.name.last}`.includes(filter))
     .map(l => l.login.username)
